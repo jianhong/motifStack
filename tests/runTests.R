f9666e48dbf56a81e458e7732b6bf923d14fabb8
Jianhong Ou <jianhong.ou@umassmed.edu>
2014-06-16 16:06:56 +0000

    Fix bugs using motifDB in tests
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/motifStack@91458 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/runTests.R
+++ tests/runTests.R
@@ -1,3 +1,3 @@
 require("motifStack") || stop("unable to load Package:motifStack")
-require("MotifDb") || stop("unalble to load Package::motifDb")
+#require("MotifDb") || stop("unalble to load Package::motifDb")
 BiocGenerics:::testPackage("motifStack")
